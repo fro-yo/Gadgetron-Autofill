30de8c2502f1b64e415367dbf7a7a29f75ec08d4
Ajeya Rengarajan <ajrengar@ucsd.edu>
2016-12-14 16:58:08 +0530

    Downloads zip files

---
+++ README.md
@@ -0,0 +1,3 @@
+Remember to turn off "Ask where to save each file before downloading" in
+Chrome advanced settings as this interferes with the automatic downloading of
+the zip files for each robot.
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-      window.open ("https://www.google.com");
-  }
-);
+
+    document.getElementById("submitButton").addEventListener("click", function () {
+
+        // getting the link
+        const fileUrl = document.getElementById("jsonUrl").value;
+
+        if (fileUrl === "") {
+            var error = document.getElementById("error");
+            error.innerHTML = "Please enter url"
+        }
+
+        // getting json from link
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", fileUrl, false);
+        xhr.send();
+        let jsonArray = JSON.parse(xhr.responseText);
+
+        // downloading each zipfile
+        for (let count = 0; count < jsonArray.length; count++) {
+            zipfileUrl = jsonArray[count].zipfile;
+            chrome.downloads.download({
+                url: zipfileUrl,
+                filename: "gadgetronOrders/gadgetronOrderData_"+count+".json",
+                conflictAction: "overwrite",
+            }, function () {
+                console.log ("Done"+count)
+            });
+        }
+        
+    });
+});
